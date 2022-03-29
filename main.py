64e19a29a979a3c9ecec855914af587897b44ecb
Salivala <hsszyman@gmail.com>
2022-03-29 18:25:00 -0400

    optionally let you use an env variable for bot prefix

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
 from dotenv import load_dotenv
 
 from src.utils.bot_factory import BotFactory
 
 load_dotenv()
 
-bot = BotFactory('!')
+prefix = os.getenv('DISCORD_PREFIX')
+bot = BotFactory(prefix if prefix != "" else "!")
 bot.start()
