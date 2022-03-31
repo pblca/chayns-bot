cd713b6fe780341605af6149b5ca85bc56505c29
Salivala <hsszyman@gmail.com>
2022-03-31 11:49:21 -0400

    Janitor Functionality ( auto delete messages after message limit )  (#18)
    * #17 - added janitor functionality ( cache only )
    
    * #17 - added janitor functionality ( cache only )
    
    * #17 - added janitor functionality ( cache only )
    
    * #17 - now able to change janitor limit
    
    * #17 - fixed small bugs with multiple janitor channels operating at the same time

--- src/utils/bot_factory.py
+++ src/utils/bot_factory.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
+from src.cogs.janitor.janitor import Janitor
 from src.commands.command_handlers import CommandHandler
 from src.events.event_handlers import EventHandler
 
@@ -10,6 +11,7 @@ class BotFactory:
 
     def __init__(self, prefix: str):
         intents = discord.Intents().all()
+        self.cache: dict = {}
         self.bot = commands.Bot(command_prefix=prefix, intents=intents)
         self.event_handlers = EventHandler(self.bot)
         self.command_handlers = CommandHandler(self.bot)
@@ -17,4 +19,5 @@ class BotFactory:
     def start(self):
         self.event_handlers.initialize()
         self.command_handlers.initialize()
+        self.bot.add_cog(Janitor(self.bot, self.cache))
         self.bot.run(os.getenv('BOT_KEY'))
