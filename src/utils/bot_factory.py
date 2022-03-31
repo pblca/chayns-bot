7c81ab10521ded49575d7d6edbb7baff3011e182
Salivala <hsszyman@gmail.com>
2022-03-30 21:24:06 -0400

    #17 - added janitor functionality ( cache only )

--- src/utils/bot_factory.py
+++ src/utils/bot_factory.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
+from src.cogs.janitor.janitor import Janitor
 from src.commands.command_handlers import CommandHandler
 from src.events.event_handlers import EventHandler
 
@@ -10,11 +11,13 @@ class BotFactory:
 
     def __init__(self, prefix: str):
         intents = discord.Intents().all()
+        self.cache: dict = {}
         self.bot = commands.Bot(command_prefix=prefix, intents=intents)
-        self.event_handlers = EventHandler(self.bot)
-        self.command_handlers = CommandHandler(self.bot)
+        self.event_handlers = EventHandler(self.bot, self.cache)
+        self.command_handlers = CommandHandler(self.bot, self.cache)
 
     def start(self):
         self.event_handlers.initialize()
         self.command_handlers.initialize()
+        self.bot.add_cog(Janitor(self.bot, self.cache))
         self.bot.run(os.getenv('BOT_KEY'))
