34a036fc113af2307a58f3eb5a1878d7023c6303
Quin <68469008+KingPEPSALT@users.noreply.github.com>
2022-04-02 17:14:47 +0100

    [feature/watch] log specific user actions to specific channels foundation (and slash commands) (#14)
    * updated .gitignore
    
    * cog loads + small weird directory case fix
    
    * big commit, maybe too big; slash commands work fully, using app_commands everywhere, watch embeds added
    
    * updated requirements to discord.py version with slash commands

--- src/utils/bot_factory.py
+++ src/utils/bot_factory.py
@@ -1,43 +1,47 @@
+import asyncio
 import os
 import discord
-from discord.ext import commands
 
+from discord.ext import commands
+from discord import app_commands
 
 from src.cogs.mirror.mirror import Mirror
 from src.cogs.janitor.janitor import Janitor
 from src.commands.command_handlers import CommandHandler
 from src.events.event_handlers import EventHandler
 
+from re import split as regsplit
 
 class BotFactory:
 
     def __init__(self, prefix: str):
         intents = discord.Intents().all()
         self.cache: dict = {}
+        
         self.bot = commands.Bot(command_prefix=prefix, intents=intents)
         self.event_handlers = EventHandler(self.bot)
         self.command_handlers = CommandHandler(self.bot)
         self.bot.cache = self.cache
-
+    
     def start(self):
         initial_extensions = []
         # Go fetch py files in the nested directories within src/cogs
         for root, directories, filenames in os.walk('src/cogs'):
             for filename in filenames:
                 if filename.endswith(".py"):
-                    directory = root.split('/')[-1]
+                    directory = regsplit(r'[/\\]', root)[-1]
                     cogs_directory = directory == 'cogs'
 
-                    # discord.py finds cogs with paths represented separated by dots
                     # because we're in utils here we need to up a directory
                     # so to load the janitor cog from src/cogs/janitor/janitor.py, it needs to look like ..cogs.janitor.janitor
                     extension_prefix = "..cogs" if cogs_directory else f"..cogs.{directory}"
                     initial_extensions.append(f"{extension_prefix}.{filename[:-3]}")
-
+        
         # Here we load our extensions(cogs) listed above in [initial_extensions].
         for extension in initial_extensions:
-            self.bot.load_extension(name = extension, package = __package__)
-
+            asyncio.run(self.bot.load_extension(name = extension, package = __package__))
+            
         self.event_handlers.initialize()
         self.command_handlers.initialize()
-        self.bot.run(os.getenv('BOT_KEY'))
+        self.bot.run(os.getenv('BOT_KEY'))    
+
