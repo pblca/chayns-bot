d5d635d1500788c47b825d670c5e83b806b7912f
Salivala <hsszyman@gmail.com>
2022-04-03 02:43:50 -0400

    #40 janitor optimizations. now uses bulk delete to reduce constant api calls. defaults to deleting messages at a time once it reaches the limit.

--- src/cogs/janitor/janitor.py
+++ src/cogs/janitor/janitor.py
@@ -2,6 +2,7 @@
 import os
 import time
 from collections import deque
+from itertools import starmap, repeat
 
 import discord
 from discord.ext import commands
@@ -22,18 +23,19 @@ class Janitor(commands.Cog):
     async def on_message(self, message: discord.Message):
         if 'janitor_cache' in cache.keys() and message.channel.id in cache['janitor_cache'].keys():
             current_cache = cache['janitor_cache'][message.channel.id]
-            current_cache['messages'].append(message.id)
+            current_cache['messages'].append(message.id) # replace this with messsage_count
             if len(current_cache['messages']) > current_cache['limit']:
-                msg: discord.Message = await message.channel.fetch_message(current_cache['messages'].popleft())
-                await msg.delete()
+                for i in range(current_cache['frequency']):
+                    current_cache['messages'].popleft() # likely don't need to store these ids in cache
+                await message.channel.purge(limit=current_cache['frequency'], check=lambda x: True)
 
     @app_commands.command(name="janitor", description="Enforces a set limit of message after command execution")
     @app_commands.guilds(int(int(os.getenv('TEST_GUILD'))))
-    async def janitor(self, interaction: Interaction, limit: int = 200):
+    async def janitor(self, interaction: Interaction, limit: int = 200, frequency: int = 5):
         if 'janitor_cache' not in cache.keys():
-            cache['janitor_cache']: dict = {interaction.channel_id: {'limit': limit, 'messages': deque()}}
+            cache['janitor_cache']: dict = {interaction.channel_id: {'limit': limit, 'frequency': frequency, 'messages': deque()}}
         else:
-            cache['janitor_cache'][interaction.channel_id] = {'limit': limit, 'messages': deque()}
+            cache['janitor_cache'][interaction.channel_id] = {'limit': limit, 'frequency': frequency, 'messages': deque()}
         
         await interaction.response.send_message("Janitor active.")
         time.sleep(2)
