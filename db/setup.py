24ae2a222a72bafb929a2eae67490c4df5738f8b
Salivala <hsszyman@gmail.com>
2022-04-03 15:15:22 -0400

    quick fixes ( class decorators, etc )

--- db/setup.py
+++ db/setup.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 
-from db import models, settings
+from db import settings
 
 Base = declarative_base()
 
