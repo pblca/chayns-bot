0aae650656157a119c231d2eff3e934d175c66f6
cody <byrnes.cody@gmail.com>
2022-04-04 16:55:35 -0700

    Fix for cog loader to target primary cog file

--- src/utils/bot_factory.py
+++ src/utils/bot_factory.py
@@ -35,12 +35,14 @@ class BotFactory:
                     # because we're in utils here we need to up a directory so to load the janitor cog from
                     # src/cogs/janitor/janitor.py, it needs to look like ..cogs.janitor.janitor
                     extension_prefix = "..cogs" if cogs_directory else f"..cogs.{directory}"
-                    initial_extensions.append(f"{extension_prefix}.{filename[:-3]}")
+                    if directory == filename[:-3]:
+                        initial_extensions.append(f"{extension_prefix}.{filename[:-3]}")
+
+        print(initial_extensions)
 
         # Here we load our extensions(cogs) listed above in [initial_extensions].
         for extension in initial_extensions:
             asyncio.run(self.bot.load_extension(name=extension, package=__package__))
 
         self.event_handlers.initialize()
-        self.command_handlers.initialize()
         self.bot.run(os.getenv('BOT_KEY'))
