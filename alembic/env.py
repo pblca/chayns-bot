783d2bf697a33a4f7f0a8d9845dee5b19b92b46b
cody <byrnes.cody@gmail.com>
2022-04-04 17:55:39 -0700

    Alembic configured to use env vars

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,14 @@
+import os
 from logging.config import fileConfig
 
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from db.setup import Base
 
 from alembic import context
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-TARGET_METADATA = None
+TARGET_METADATA = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -57,6 +62,11 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+    config.set_main_option(
+        'sqlalchemy.url',
+        f'postgresql+psycopg2://{os.getenv("PS_NAME")}:{os.getenv("PS_PASS")}'
+        f'@{os.getenv("PS_HOST")}/{os.getenv("PS_DB")}')
+
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
