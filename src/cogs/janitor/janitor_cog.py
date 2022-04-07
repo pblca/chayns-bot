003bdb9589512b9d597e25d33a3dac5c79b0b97c
Salivala <hsszyman@gmail.com>
2022-04-06 20:19:51 -0400

    big meaty changes. added redis commander to docker-compose.yml to make cache debugging easier. Updated discord.py commit number to support more threading commands. moved janitor over to using redis. is this one ticket going to include a million changes? yes.

--- src/cogs/janitor/janitor_cog.py
+++ src/cogs/janitor/janitor_cog.py
@@ -4,13 +4,14 @@ import time
 import discord
 import sqlalchemy.orm
 from discord.ext import commands
-from discord import app_commands, Interaction
+from discord import app_commands, Interaction, Thread
 
 from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 
 from data.cache import cache
 from db.models import Channel
+from src.utils.connectors import r
 
 load_dotenv()
 
@@ -24,24 +25,56 @@ class Janitor(commands.Cog):
     async def on_ready(self):
         session: sqlalchemy.orm.Session = sessionmaker(bind=self.bot.engine)()
         channels = filter(lambda _channel: _channel.janitor_limit is not None, session.query(Channel).all())
-        cache['janitor_cache'] = {}
         count = 0
         for channel in channels:
-            cache['janitor_cache'][channel.id] = \
-                {'limit': channel.janitor_limit, 'frequency': channel.janitor_frequency, 'message_count': [0]}
+            try:
+                discord_channel = await self.bot.fetch_channel(channel.id)
+                for thread in discord_channel.threads:
+                    r.hset(f'janitor_cache:{channel.id}', f'message_count:{thread.id}', 0)
+                    # I AM SO SORRY THIS IS LITERALLY THE ONLY WAY
+                    try:
+                        await thread.purge(limit=None, check=lambda m: not m.is_system())
+                    except discord.errors.Forbidden:
+                        print('life is pain')
+            except discord.errors.NotFound as exception:
+                print('channel deleted while offline')
+                session.delete(channel)
+                session.commit()
+            r.hset(f'janitor_cache:{channel.id}', 'limit', channel.janitor_limit)
+            r.hset(f'janitor_cache:{channel.id}', 'frequency', channel.janitor_frequency)
+            r.hset(f'janitor_cache:{channel.id}', 'message_count', 0)
             count += 1
         print(f'Janitor managing {count} channel(s)')
         session.close()
 
     @commands.Cog.listener()
+    async def on_thread_create(self, thread):
+        if current_cache := r.hgetall(f'janitor_cache:{thread.parent_id}'):
+            r.hset(f'janitor_cache:{thread.parent_id}', f'message_count:{thread.id}', 0)
+
+    @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        match cache:
-            case {'janitor_cache': {message.channel.id: current_cache}}:
-                count = current_cache['message_count']
-                count[0] += 1
-                if count[0] >= current_cache['limit']:
-                    count[0] -= current_cache['frequency']
-                    await message.channel.purge(limit=current_cache['frequency'], check=lambda x: True, oldest_first=True)
+        if type(message.channel) == discord.Thread:
+            thread = message.channel
+            if current_cache := r.hgetall(f'janitor_cache:{message.channel.parent_id}'):
+                count = int(current_cache[f'message_count:{thread.id}'])
+                r.hset(f'janitor_cache:{thread.parent_id}', f'message_count:{thread.id}', count+1)
+                if count+1 > int(current_cache['limit']):
+                    count -= int(current_cache['frequency'])
+                    r.hset(f'janitor_cache:{thread.parent_id}', f'message_count:{thread.id}', count)
+                    await message.channel.purge(limit=int(current_cache['frequency']),
+                                                check=lambda x: True,
+                                                oldest_first=True)
+        else:
+            if current_cache := r.hgetall(f'janitor_cache:{message.channel.id}'):
+                count = int(current_cache['message_count'])
+                r.hset(f'janitor_cache:{message.channel.id}', 'message_count', count+1)
+                if count+1 >= int(current_cache['limit']):
+                    count -= int(current_cache['frequency'])
+                    r.hset(f'janitor_cache:{message.channel.id}', 'message_count',  count-int(current_cache['frequency']))
+                    await message.channel.purge(limit=int(current_cache['frequency']),
+                                                check=lambda x: True,
+                                                oldest_first=True)
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message):
@@ -52,7 +85,6 @@ class Janitor(commands.Cog):
     @app_commands.command(name="janitor", description="Enforces a set limit of message after command execution")
     @app_commands.guilds(int(int(os.getenv('TEST_GUILD'))))
     async def janitor(self, interaction: Interaction, limit: int = 200, frequency: int = 5):
-        janitor_information = {'limit': limit, 'frequency': frequency, 'message_count': [0]}
         session: sqlalchemy.orm.Session = sessionmaker(bind=self.bot.engine)()
         channel = session.query(Channel).get(interaction.channel_id)
         if channel is None:
@@ -63,15 +95,16 @@ class Janitor(commands.Cog):
             session.add(channel)
             session.commit()
 
-        match cache:
-            case {"janitor_cache": _}:
-                cache['janitor_cache'][interaction.channel_id] = janitor_information
-            case _:
-                cache['janitor_cache']: dict = {interaction.channel_id: janitor_information}
-                channel.janitor_limit = limit
-                channel.janitor_frequency = frequency
-                session.commit()
+        r.hset(f'janitor_cache:{interaction.channel_id}', 'limit', limit)
+        r.hset(f'janitor_cache:{interaction.channel_id}', 'frequency', frequency)
+        r.hset(f'janitor_cache:{interaction.channel_id}', 'message_count', 0)
+        discord_channel = await self.bot.fetch_channel(interaction.channel_id)
+        for thread in discord_channel.threads:
+            r.hset(f'janitor_cache:{interaction.channel_id}', f'message_count:{thread.id}', 0)
 
+        channel.janitor_limit = limit
+        channel.janitor_frequency = frequency
+        session.commit()
         session.close()
 
         await interaction.response.send_message("Janitor active.")
